820a0bca20dc8d86e741a55b255f13635b0e6b96
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-22 01:10:25 -0300

    fix: ajustando shaerd-metadata

--- src/app/shared-metadata.tsx
+++ src/app/shared-metadata.tsx
@@ -1,34 +1,39 @@
 import data from '@/data/user.json';
 
 export const sharedMetadata = {
+  metadataBase: new URL("https://bio-link-green-two.vercel.app"),
+  
   title: `${data.profile.nickname} | BioLink`,
   description: data.profile.bio,
+
   openGraph: {
     title: `${data.profile.nickname} - Links e Contato`,
     description: data.profile.bio,
-    url: 'https://bio-link-green-two.vercel.app/',
-    siteName: 'BioLink',
+    url: "https://bio-link-green-two.vercel.app/",
+    siteName: "BioLink",
     images: [
       {
-        url: data.profile.profileImage || '/default-image.jpg',
+        url: data.profile.profileImage || "/default-image.jpg",
         width: 800,
         height: 600,
       },
     ],
-    locale: 'pt_BR',
-    type: 'website',
+    locale: "pt_BR",
+    type: "website",
   },
-   icons: {
+
+  icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/icon-16x16.png', type: 'image/png', sizes: '16x16' },
-      { url: '/icon-32x32.png', type: 'image/png', sizes: '32x32' },
-      { url: '/android-chrome-192x192.png', type: 'image/png', sizes: '192x192' },
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon-16x16.png", type: "image/png", sizes: "16x16" },
+      { url: "/icon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/android-chrome-192x192.png", type: "image/png", sizes: "192x192" },
     ],
-    shortcut: ['/favicon.ico'],
+    shortcut: ["/favicon.ico"],
     apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
   },
-  manifest: '/site.webmanifest',
+
+  manifest: "/site.webmanifest",
 };
